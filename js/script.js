cb78d7df0fdb2475dcd1e2070dbdafdd86e9ea10
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-09-30 19:47:32 -0300

    feat: Capturando o primeiro Parágrafo e todos os parágrafos

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ console.log(meuDiv);
 
 let paragrafos = document.getElementsByClassName('paragrafo');
 console.log(paragrafos);
+
+let primeiroParagrafo = document.querySelector('.paragrafo');
+console.log(primeiroParagrafo);
+
+let todosParagrafos = document.querySelectorAll('.paragrafo');
+console.log(todosParagrafos);
