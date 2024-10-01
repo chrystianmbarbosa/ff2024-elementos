6c56dda638f9fb0eab620a4a20c689d6fb639df8
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-09-30 22:00:03 -0300

    feat: Exercício 2 Corrigido

--- exercicios/ex2/js/script.js
+++ exercicios/ex2/js/script.js
@@ -1,11 +1,20 @@
-let meuDiv = document.getElementById('meuDiv');
-console.log(meuDiv);
+/*Crie dois inputs de tipo número e ao clicar no botão
+ “Somar” imprima os números informados e a 
+ soma desses valores num elemento “p” 
+ no formato “1 + 2 = 3”.*/
 
-let paragrafos = document.getElementsByClassName('paragrafo');
-console.log(paragrafos);
-
-let primeiroParagrafo = document.querySelector('.paragrafo');
-console.log(primeiroParagrafo);
-
-let todosParagrafos = document.querySelectorAll('.paragrafo');
-console.log(todosParagrafos);
+function somar() {
+    let numero1 =
+        document.querySelector('.num1').value;
+    let numero2 =
+        document.querySelector('.num2').value;
+    numero1 = parseInt(numero1);
+    numero2 = parseInt(numero2);
+    let soma = numero1 + numero2;
+    pResultado =
+        document.querySelector("#resultado");
+    pResultado.innerHTML =
+        `${numero1} + ${numero2} = ${soma}`;
+}
+let botao = document.querySelector("button");
+botao.addEventListener('click', somar);
